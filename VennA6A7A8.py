dae052e41b25f7748b750459dd048d0df40c6efd
yiwenbu <wybyw@hotmail.com>
2018-08-24 00:08:02 -0500

    analyze ezh2 knockdown

--- VennA6A7A8.py
+++ VennA6A7A8.py
@@ -1,28 +1,29 @@
 """
 Compare A6, A7, A8
 EZH2 knockdown
+output : "WTvsEZH2sh/A7vsCtrl_final.xls"
 """
 
 
 import pandas as pd
+import numpy as np
 from matplotlib_venn import venn3, venn3_circles
 from matplotlib import pyplot as plt
+pd.set_option('display.max_columns', 50)
 
 
-def get_list(input_f):
-    df = pd.read_csv(input_f, sep="\t")
+def get_list(df):
     chrom_pos = df["chr_pos"]
     chrom_pos.drop_duplicates(inplace=True)
     l_position = chrom_pos.tolist()
     return l_position
 
 
-def get_gene_list(input_f):
-    df = pd.read_csv(input_f, sep="\t")
-    dfs = df[df["gene"]!="intergenic"]
-    df_gene = dfs["gene"]
-    df_gene.drop_duplicates(inplace=True)
-    return df_gene
+def get_gene_df(df,genelist):
+    dfs = df[df["chr_pos"].isin(genelist)]
+    dfs = dfs[["chr_pos", "gene", "annot1", "alu?",	"non_alu_repetitive?"]]
+    dfs.drop_duplicates(inplace=True)
+    return dfs
 
 
 
@@ -33,34 +34,93 @@ def draw_venn3(lst1,lst2,lst3):
     pass
 
 
-def call_venn():
-    # RNA editing sites
-    lst1=get_list("EZH2Rediting/A6all1.xls")
-    lst2=get_list("EZH2Rediting/A7all1.xls")
-    lst3=get_list("EZH2Rediting/A8all1.xls")
-    # draw_venn3(lst1,lst2,lst3)
-    print "editing sites:", len(lst1), len(lst2), len(lst3)
-
-    # RNA editing genes
-    genelst1=get_gene_list("EZH2Rediting/A6all1.xls")
-    genelst2=get_gene_list("EZH2Rediting/A7all1.xls")
-    genelst3=get_gene_list("EZH2Rediting/A8all1.xls")
-    draw_venn3(genelst1,genelst2,genelst3)
-    print "editing genes:", len(genelst1), len(genelst2), len(genelst3)
-    return lst1,lst2,lst3,genelst1,genelst2,genelst3
+def get_gene_list(df):
+    gene_list = list(set(df["gene"].tolist()))
+    return gene_list
 
 
+def genelist_count(file, exclude_list):
+    A = pd.read_csv(file, sep="\t")
+    A = A[~A["gene"].isin(exclude_list)]
+    A_df = A.groupby("gene").size()
+    A_df.sort_values(inplace=True, ascending=False)
+    A_df.drop("intergenic", axis=0, inplace=True)
+    return A_df
 
-def diff_genes():
 
-
-
-
-
+def get_utr(file, genelist):
+    genelist1 = genelist.index
+    df = pd.read_csv(file, sep="\t")
+    df_list = df[df["gene"]==genelist1]
+    df_list_s = df_list[["gene", "annot1"]]
     pass
 
+
+def add_annot(df, comb_table):
+    df2 = df.groupby(["gene", "annot1"]).size()
+    UTR3 = df2[:, "3UTR"]
+    UTR5 = df2[:, "5UTR"]
+    INTRON = df2[:, "intronic"]
+    df3 = pd.concat([comb_table,UTR3, UTR5, INTRON], axis=1, join="outer", sort=True)
+    df3.rename({0: "total_edits", 1: "3UTR", 2:"5UTR", 3:"intronic"}, inplace=True)
+    df3.replace(" ", np.nan, inplace=True)
+    df3.fillna(0, inplace=True)
+    return df3
+
+
+
 def main():
-    lst1,lst2,lst3, glst1,glst2,glst3= call_venn()
+    A6_df=pd.read_csv("EZH2Rediting/A6all1.xls", sep="\t")
+    A7_df=pd.read_csv("EZH2Rediting/A7all1.xls", sep="\t")
+    A8_df=pd.read_csv("EZH2Rediting/A8all1.xls", sep="\t")
+    lst1=get_list(A6_df)
+    lst2=get_list(A7_df)
+    lst3=get_list(A8_df)
+    # draw_venn3(lst1,lst2,lst3)
+
+    gained = list((set(lst3).union(lst2)).difference(lst1))
+    lost =  list(set(lst1).difference(set(lst3).union(lst2)))
+
+    A7uniq_df = get_gene_df(A7_df, gained)
+    A8uniq_df = get_gene_df(A8_df, gained)
+    # A7uniq_df.to_csv("WTvsEZH2sh/A7gained.xls", sep="\t")
+    # A8uniq_df.to_csv("WTvsEZH2sh/A8gained.xls", sep="\t")
+    A6uniq_df = get_gene_df(A6_df,lost)
+    # A6uniq_df.to_csv("WTvsEZH2sh/A6gained.xls", sep="\t")
+
+    A7_gained_list = get_gene_list(A7uniq_df)
+    A8_gained_list = get_gene_list(A8uniq_df)
+    A6_lost_list = get_gene_list(A6uniq_df)
+    # draw_venn3(A6_lost_list,A7_gained_list,A8_gained_list)
+
+    A7_table = A7uniq_df.groupby("gene").size()
+    A8_table = A8uniq_df.groupby("gene").size()
+    A6_table = A6uniq_df.groupby("gene").size()
+    # A7_table.to_csv("WTvsEZH2sh/A7grouped.xls", sep="\t", header=False)
+    # A8_table.to_csv("WTvsEZH2sh/A8grouped.xls", sep="\t", header=False)
+    # A6_table.to_csv("WTvsEZH2sh/A6grouped.xls", sep="\t", header=False)
+
+    # print A7uniq_df["annot1"].unique()
+
+    A6_distrib = A6_df.groupby("annot1").size()
+    A7_distrib = A7_df.groupby("annot1").size()
+    A8_distrib = A8_df.groupby("annot1").size()
+    # print A6_distrib
+    distrib = pd.DataFrame()
+    distrib["control"] = A6_distrib
+    distrib["EZH2_sh1"]= A7_distrib
+    distrib["Ezh2_sh2"]= A8_distrib
+    # distrib.to_csv("WTvsEZH2sh/distribution.xls", sep="\t")
+
+    A7_final = add_annot(A7uniq_df, A7_table)
+    A8_final = add_annot(A8uniq_df, A8_table)
+    A6_final = add_annot(A6uniq_df, A6_table)
+
+    A7_final.to_csv("WTvsEZH2sh/A7vsCtrl_final.xls", sep="\t")
+    A8_final.to_csv("WTvsEZH2sh/A8vsCtrl_final.xls", sep="\t")
+    A6_final.to_csv("WTvsEZH2sh/CtrvsA7A8_final.xls", sep="\t")
+
+
 
     pass
 
